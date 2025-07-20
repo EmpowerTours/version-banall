51203fcca80c9cdf83325cd4de6a37f9318f97a8
EmpowerTours <162839159+EmpowerTours@users.noreply.github.com>
2025-07-20 00:59:47 -0600

    Update next.config.js

--- farcaster/next.config.js
+++ farcaster/next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_BASE_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+      {
+        source: '/images/:path*',
+        destination: '/public/images/:path*',
       },
     ];
   },
